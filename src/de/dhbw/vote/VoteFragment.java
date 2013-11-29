f2f31980e7769681da418fab68ebc5f8131cb0e3
Mark Atzmüller <mark.atzmueller@gmx.de>
2013-11-29 12:30:15 +0100

    load Image in VoteFragment, improve layout

--- src/de/dhbw/vote/VoteFragment.java
+++ src/de/dhbw/vote/VoteFragment.java
@@ -1,10 +1,13 @@
 package de.dhbw.vote;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,7 @@ import android.webkit.WebViewClient;
 import android.webkit.WebViewFragment;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,19 +34,21 @@ import de.dhbw.navigation.R;
 public class VoteFragment extends Fragment {
 
     private Context mContext;
+    private View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         mContext = getActivity();
 
-        View view = inflater.inflate(R.layout.fragment_vote, container, false);
+        mView = inflater.inflate(R.layout.fragment_vote, container, false);
 
-        WebView webView = (WebView) view.findViewById(R.id.vote_webview);
+        WebView webView = (WebView) mView.findViewById(R.id.vote_webview);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(mContext), "WebApp");
 
-        EditText nameField = (EditText) view.findViewById(R.id.vote_edittext_name);
-        EditText captchaField = (EditText) view.findViewById(R.id.vote_edittext_captcha);
+        EditText nameField = (EditText) mView.findViewById(R.id.vote_edittext_name);
+        EditText captchaField = (EditText) mView.findViewById(R.id.vote_edittext_captcha);
 
         nameField.setText("Name");  //TODO: Hier Name aus Einstellungen einfügen
 
@@ -51,44 +57,80 @@ public class VoteFragment extends Fragment {
             public void onPageFinished(WebView view, String url) {
                 Log.d("TestOnPageFinished", url);
 
-                view.loadUrl("javascript:(function(){var intervalID = setInterval(function() {\n" +
-                        "\tif (document.getElementById('recaptcha_image') != undefined)\n" +
-                        "\t{\n" +
-                        "\t\talert(document.getElementById('recaptcha_image').firstChild.src);\n" +
-                        "\t\tclearInterval(intervalID);\n" +
-                        "\t}\n" +
+                view.loadUrl("javascript:(function(){var i=0;\n" +
+                        "var intervalID = setInterval(function() \n" +
+                        "{\n" +
+                        "if (document.getElementById('recaptcha_image') != undefined)\n" +
+                        "{\n" +
+                        "WebApp.loadImage(document.getElementById('recaptcha_image').firstChild.src);\n" +
+                        "clearInterval(intervalID);\n" +
+                        "}\n" +
+                        "else\n" +
+                        "{\n" +
+                        "i++;\n" +
+                        "if (i>3)\n" +
+                        "WebApp.error();\n" +
+                        "}\n" +
                         "}, 1000);})()");
                 super.onPageFinished(view, url);
             }
         });
 
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-
-                try
-                {
-                    Log.d("TestOnJSAlert", url);
-                    ImageView imageView = (ImageView) view.findViewById(R.id.vote_image_captcha);
-                    Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(message).getContent());
-                    imageView.setImageBitmap(bitmap);
-                }
-                catch (MalformedURLException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-                return super.onJsAlert(view, url, message, result);
-            }
-        });
-
         webView.loadUrl("http://minecraft-server.eu/?go=servervote&id=2421");
-
         Log.d("TestLoadUrl", "Load URL");
 
-        return view;
+        return mView;
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        public void loadImage(String url) {
+            ImageView imageView = (ImageView) mView.findViewById(R.id.vote_image_captcha);
+            new DownloadImageTask(imageView).execute(url);
+            imageView.setBackgroundResource(0);
+        }
+
+        public void error() {
+            ImageView imageView = (ImageView) mView.findViewById(R.id.vote_image_captcha);
+            imageView.setImageResource(R.drawable.ic_link_ban);
+            imageView.setBackgroundResource(0);
+            Toast.makeText(mContext, "Captcha-Bild konnte nicht geladen werden", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        private ImageView bmImage;
+
+        public DownloadImageTask(ImageView imageView) {
+            this.bmImage = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urls[0]).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", "image download error");
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            //set image of your imageview
+            bmImage.setImageBitmap(result);
+            Log.d("Test", "Task closed");
+        }
     }
 }
