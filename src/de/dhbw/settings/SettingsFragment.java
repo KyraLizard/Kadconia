5ba6d7a9a51d3f9a4d053a695eb060b4113ac6d9
Mark Atzmüller <mark.atzmueller@gmx.de>
2014-04-02 23:33:28 +0200

    Add Vote Notification

--- src/de/dhbw/settings/SettingsFragment.java
+++ src/de/dhbw/settings/SettingsFragment.java
@@ -1,9 +1,28 @@
 package de.dhbw.settings;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.Calendar;
+
+import de.dhbw.navigation.NavigationActivity;
 import de.dhbw.navigation.R;
+import de.dhbw.vote.VoteFragment;
 
 /**
  * Created by Mark on 18.11.13.
@@ -17,4 +36,39 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Preference voteNotification = findPreference(getString(R.string.pref_vote_notification_key));
+        if (voteNotification != null) {
+            voteNotification.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+
+                    Context context = getActivity();
+                    Intent intent = new Intent(context, VoteNotification.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                    // Returns state before it is changed
+                    if (((CheckBoxPreference) preference).isChecked() == true)
+                    {
+                        ((AlarmManager) context.getSystemService(Context.ALARM_SERVICE)).cancel(pendingIntent);
+                    }
+                    else
+                    {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTimeInMillis(System.currentTimeMillis());
+                        calendar.set(Calendar.HOUR_OF_DAY, 17);
+                        ((AlarmManager) context.getSystemService(Context.ALARM_SERVICE)).setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),(1000*60*60*24), pendingIntent);
+                    }
+
+                    return true;
+                }
+            });
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
+
---
+++ src/de/dhbw/settings/VoteNotification.java
@@ -0,0 +1,40 @@
+package de.dhbw.settings;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import de.dhbw.navigation.NavigationActivity;
+import de.dhbw.navigation.R;
+
+/**
+ * Created by Mark on 02.04.2014.
+ */
+public class VoteNotification extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_kadcon_logo_alternate)
+                        .setContentTitle(context.getString(R.string.notification_title_vote))
+                        .setContentText(context.getString(R.string.notification_text_vote) + (new SimpleDateFormat("HH:mm:ss")).format(new Date()));
+        Intent resultIntent = new Intent(context, NavigationActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(NavigationActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(Integer.parseInt(context.getString(R.string.notification_id_vote)), mBuilder.build());
+    }
+}
